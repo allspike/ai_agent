082251dd797662ff07dd94cd51f3c5ac894c544e
allspike <jspikerman@icloud.com>
2025-08-24 10:22:52 -0700

    Finished get_file_content function and updated tests to verify intended behavior

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,23 @@
 import os
 
 def get_file_content(working_directory, file_path):
-    
+    absolute = os.path.abspath(os.path.join(working_directory, file_path))
+    if not absolute.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(absolute):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    MAX_CHARS = 10000
+    trunc_message = f'[...File "{file_path}" truncated at 10000 characters]'
+    try:
+        with open(absolute, "r") as f:
+            file_content_string = f.read()
+            if len(file_content_string) > MAX_CHARS:
+                f.seek(0)
+                trunc_content = f.read(MAX_CHARS)
+                return trunc_content + '\n' +  trunc_message
+            else:
+                return file_content_string
+    except Exception as e:
+        return f'Error: {e}'
+            
+            
--- tests.py
+++ tests.py
@@ -1,26 +1,50 @@
 # tests.py
-from functions.get_files_info import get_files_info
+# from functions.get_files_info import get_files_info
 
-def current_test():
-    print("Result for current directory:")
-    print(get_files_info("calculator", "."))
+from functions.get_file_content import get_file_content
+# def current_test():
+#    print("Result for current directory:")
+#    print(get_files_info("calculator", "."))
 
-def pkg_test():
-    print("Result for 'pkg' directory:")
-    print(get_files_info("calculator", "pkg"))
+# def pkg_test():
+#    print("Result for 'pkg' directory:")
+#    print(get_files_info("calculator", "pkg"))
 
-def bin_test():
-    print("Result for '/bin' directory:")
-    print(get_files_info("calculator", "/bin"))
+# def bin_test():
+#    print("Result for '/bin' directory:")
+#    print(get_files_info("calculator", "/bin"))
 
-def illegal_test():
-    print("Result for '../' directory'")
-    print(get_files_info("calculator", "../"))
+# def illegal_test():
+#    print("Result for '../' directory'")
+#    print(get_files_info("calculator", "../"))
+
+# def main():
+#    current_test()
+#    pkg_test()
+#    bin_test()
+#    illegal_test()
+
+def main_test():
+    print(get_file_content("calculator", "main.py"))
+        
+def pkg_calc_test():
+    print(get_file_content("calculator", "pkg/calculator.py"))
+
+def cat_test():
+    print(get_file_content("calculator", "/bin/cat"))
+
+def dne_test():
+    print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
+def lorem_test():
+    print(get_file_content("calculator", "lorem.txt"))
 
 def main():
-    current_test()
-    pkg_test()
-    bin_test()
-    illegal_test()
+    main_test()
+    pkg_calc_test()
+    cat_test()
+    dne_test()
+    lorem_test()
+        
 if __name__ == "__main__":
     main()
